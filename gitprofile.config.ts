b4396aa165cf8795510c67e132e822b9e17bcf5f
khabon <kimberlyhabon@yahoo.com>
2025-04-21 10:30:00 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -52,7 +52,7 @@ const CONFIG = {
     },
   },
   seo: {
-    title: 'Kimberly Habon Website,
+    title: 'Kimberly Habon Website',
     description: '',
     imageURL: '',
   },
@@ -80,7 +80,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/khabon/khabon.github.io/raw/main/KimCV.pdf, // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
